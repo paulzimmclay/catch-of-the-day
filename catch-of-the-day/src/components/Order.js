3ab8d9a0bcac6f5b81e6045283a73214f160474b
pzc <paul.zimmerman.clayton@gmail.com>
2018-06-02 18:18:17 -0500

    first commit

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Inventory from "./Inventory"
+
+class Order extends React.Component {
+  render() {
+    return (
+      <div className="order">
+        <p className="order">Order!</p>
+      </div>
+    )
+  }
+}
+
+export default Order;
